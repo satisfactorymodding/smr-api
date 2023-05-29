cc9bc6212a9d11adb7cc47f363fc4dba453b75d3
porisius <47161774+porisius@users.noreply.github.com>
2023-05-29 19:02:09 +0000

    feat: VirusTotal parallelization

--- validation/virustotal.go
+++ validation/virustotal.go
@@ -9,6 +9,7 @@ import (
 	"github.com/pkg/errors"
 	"github.com/rs/zerolog/log"
 	"github.com/spf13/viper"
+	"golang.org/x/sync/errgroup"
 )
 
 var client *vt.Client
@@ -32,44 +33,82 @@ type AnalysisResults struct {
 }
 
 func ScanFiles(ctx context.Context, files []io.Reader, names []string) (bool, error) {
-	for i, file := range files {
-		scan, err := client.NewFileScanner().Scan(file, names[i], nil)
-		if err != nil {
-			return false, errors.Wrap(err, "failed to scan file")
-		}
+	errs, gctx := errgroup.WithContext(context.Background())
+	fileCount := len(files)
 
-		analysisID := scan.ID()
+	c := make(chan bool)
 
-		log.Info().Msgf("uploaded virus scan for file %s and analysis ID: %s", names[i], analysisID)
-
-		for {
-			time.Sleep(time.Second * 15)
-
-			var target AnalysisResults
-			_, err = client.GetData(vt.URL("analyses/%s", analysisID), &target)
-
+	for i := 0; i < fileCount; i++ {
+		count := i
+		errs.Go(func() error {
+			ok, err := scanFile(gctx, files[count], names[count])
 			if err != nil {
-				return false, errors.Wrap(err, "failed to get analysis results")
+				return errors.Wrap(err, "failed to scan file")
 			}
+			c <- ok
+			return nil
+		})
+	}
+	go func() {
+		_ = errs.Wait()
+		close(c)
+	}()
 
-			if target.Attributes.Status != "completed" {
-				continue
-			}
-
-			if target.Attributes.Stats == nil {
-				return false, nil
-			}
-
-			if target.Attributes.Stats.Malicious == nil || target.Attributes.Stats.Suspicious == nil {
-				return false, nil
-			}
-
-			if *target.Attributes.Stats.Malicious > 0 || *target.Attributes.Stats.Suspicious > 0 {
-				return false, nil
-			}
-
+	success := true
+	for res := range c {
+		if !res {
+			success = false
 			break
 		}
+	}
+
+	if err := errs.Wait(); err != nil {
+		return false, errors.Wrap(err, "failed to scan file")
+	}
+
+	return success, nil
+}
+
+func scanFile(ctx context.Context, file io.Reader, name string) (bool, error) {
+	scan, err := client.NewFileScanner().Scan(file, name, nil)
+	if err != nil {
+		return false, errors.Wrap(err, "failed to scan file")
+	}
+
+	analysisID := scan.ID()
+
+	log.Info().Msgf("uploaded virus scan for file %s and analysis ID: %s", name, analysisID)
+
+	for {
+		time.Sleep(time.Second * 15)
+
+		var target AnalysisResults
+		_, err = client.GetData(vt.URL("analyses/%s", analysisID), &target)
+
+		if err != nil {
+			return false, errors.Wrap(err, "failed to get analysis results")
+		}
+
+		if target.Attributes.Status != "completed" {
+			continue
+		}
+
+		if target.Attributes.Stats == nil {
+			log.Error().Msgf("no stats available. failing file: %s", name)
+			return false, nil
+		}
+
+		if target.Attributes.Stats.Malicious == nil || target.Attributes.Stats.Suspicious == nil {
+			log.Error().Msgf("unable to determine malicious or suspicious File: %s", name)
+			return false, nil
+		}
+
+		if *target.Attributes.Stats.Malicious > 0 || *target.Attributes.Stats.Suspicious > 0 {
+			log.Error().Msgf("suspicious or malicious file found: %s", name)
+			return false, nil
+		}
+
+		break
 	}
 
 	return true, nil
