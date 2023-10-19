c13a52bfa52a4c1aec3a5488e2a37d53e68c84f4
Vilsol <me@vil.so>
2023-10-19 21:43:47 +0300

    chore: reorder go generate steps

--- tools.go
+++ tools.go
@@ -6,6 +6,6 @@ package smr
 import _ "github.com/99designs/gqlgen"
 import _ "github.com/swaggo/swag/cmd/swag"
 
+//go:generate protoc -I./proto --go_out=./proto --go_opt=paths=source_relative --go-grpc_out=./proto --go-grpc_opt=paths=source_relative proto/parser/parser.proto
 //go:generate go run github.com/99designs/gqlgen generate
 //go:generate go run github.com/swaggo/swag/cmd/swag init --generalInfo cmd/api/serve.go
-//go:generate protoc -I./proto --go_out=./proto --go_opt=paths=source_relative --go-grpc_out=./proto --go-grpc_opt=paths=source_relative proto/parser/parser.proto
