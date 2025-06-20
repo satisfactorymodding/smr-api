84aa93df7814fa712df8c87f96b7911caa383367
Vilsol <me@vil.so>
2025-06-21 02:19:00 +0300

    chore: add back sleep, skip vt test if missing

--- tests/version_test.go
+++ tests/version_test.go
@@ -77,8 +77,8 @@ func RunVersionTestWrapper(t *testing.T, modFilePath string, executeVirusCheck b
 
 func RunVersionTest(ctx context.Context, t *testing.T, client *graphql.Client, modFilePath string, executeVirusCheck bool, modReference string, reuseModID string, expectError string) string {
 	if executeVirusCheck && (!viper.IsSet("virustotal.key") || viper.GetString("virustotal.key") == "") {
-		println("missing virustotal key from config")
-		t.FailNow()
+		println("missing virustotal key from config, skipping")
+		t.SkipNow()
 		return ""
 	}
 
