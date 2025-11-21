7b96fac04d8df04a4c943fb507104579a3316da3
Rob B <computerguy440+gh@gmail.com>
2025-11-21 14:30:35 -0500

    test: fix tests locally on windows, additional dev setup directions (#129)
    * chore: try running tests on windows runner too
    
    * chore: turn off running tests on windows runners (fails, seems like it tries to run other containers as windows)
    
    * fix: multi target upload true in sample config so tests pass out the gates
    
    * fix: make logging test platform-agnostic
    
    * chore: additional setup directions, improved local testing experience

--- logging/log_test.go
+++ logging/log_test.go
@@ -2,14 +2,11 @@ package logging
 
 import (
 	"bytes"
-	"context"
-	"log/slog"
+	"path/filepath"
 	"regexp"
-	"sync"
 	"testing"
 
 	"github.com/MarvinJWendt/testza"
-	"github.com/Vilsol/slox"
 	"github.com/spf13/viper"
 )
 
@@ -18,29 +15,18 @@ func TestLoggingDev(t *testing.T) {
 
 	testza.AssertNoError(t, SetupLogger(buffer))
 
-	slog.Info("A: 1")
-	slox.Info(context.Background(), "B: 2")
-
-	func() {
-		slox.Info(context.Background(), "C: 3")
-	}()
-
-	var wg sync.WaitGroup
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		slox.Info(context.Background(), "D: 4")
-	}()
-	wg.Wait()
+	PrintLoggingDummies()
 
 	out := regexp.MustCompile(`\x1b\[\d+m`).ReplaceAll(buffer.Bytes(), []byte{})
 
 	lines := bytes.Split(out, []byte("\n"))
 
-	testza.AssertContains(t, string(lines[0]), "INF logging/log_test.go:21 A: 1 service=api")
-	testza.AssertContains(t, string(lines[1]), "INF logging/log_test.go:22 B: 2 service=api")
-	testza.AssertContains(t, string(lines[2]), "INF logging/log_test.go:25 C: 3 service=api")
-	testza.AssertContains(t, string(lines[3]), "INF logging/log_test.go:32 D: 4 service=api")
+	path := filepath.Join("logging", "logging_dummies.go")
+
+	testza.AssertContains(t, string(lines[0]), "INF "+path+":16 A: 1 service=api")
+	testza.AssertContains(t, string(lines[1]), "INF "+path+":17 B: 2 service=api")
+	testza.AssertContains(t, string(lines[2]), "INF "+path+":20 C: 3 service=api")
+	testza.AssertContains(t, string(lines[3]), "INF "+path+":27 D: 4 service=api")
 
 	if t.Failed() {
 		println(buffer.String())
@@ -53,27 +39,14 @@ func TestLoggingProd(t *testing.T) {
 
 	testza.AssertNoError(t, SetupLogger(buffer))
 
-	slog.Info("A: 1")
-	slox.Info(context.Background(), "B: 2")
-
-	func() {
-		slox.Info(context.Background(), "C: 3")
-	}()
-
-	var wg sync.WaitGroup
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		slox.Info(context.Background(), "D: 4")
-	}()
-	wg.Wait()
+	PrintLoggingDummies()
 
 	lines := bytes.Split(buffer.Bytes(), []byte("\n"))
 
-	testza.AssertTrue(t, regexp.MustCompile(`\{"time":".+?","level":"INFO","source":\{"function":"github.com/satisfactorymodding/smr-api/logging.TestLoggingProd","file":".*?/logging/log_test.go","line":56},"msg":"A: 1"}`).Match(lines[0]))
-	testza.AssertTrue(t, regexp.MustCompile(`\{"time":".+?","level":"INFO","source":\{"function":"github.com/satisfactorymodding/smr-api/logging.TestLoggingProd","file":".*?/logging/log_test.go","line":57},"msg":"B: 2"}`).Match(lines[1]))
-	testza.AssertTrue(t, regexp.MustCompile(`\{"time":".+?","level":"INFO","source":\{"function":"github.com/satisfactorymodding/smr-api/logging.TestLoggingProd.func1","file":".*?/logging/log_test.go","line":60},"msg":"C: 3"}`).Match(lines[2]))
-	testza.AssertTrue(t, regexp.MustCompile(`\{"time":".+?","level":"INFO","source":\{"function":"github.com/satisfactorymodding/smr-api/logging.TestLoggingProd.func2","file":".*?/logging/log_test.go","line":67},"msg":"D: 4"}`).Match(lines[3]))
+	testza.AssertTrue(t, regexp.MustCompile(`\{"time":".+?","level":"INFO","source":\{"function":"github.com/satisfactorymodding/smr-api/logging.PrintLoggingDummies","file":".*?/logging/logging_dummies.go","line":16},"msg":"A: 1"}`).Match(lines[0]))
+	testza.AssertTrue(t, regexp.MustCompile(`\{"time":".+?","level":"INFO","source":\{"function":"github.com/satisfactorymodding/smr-api/logging.PrintLoggingDummies","file":".*?/logging/logging_dummies.go","line":17},"msg":"B: 2"}`).Match(lines[1]))
+	testza.AssertTrue(t, regexp.MustCompile(`\{"time":".+?","level":"INFO","source":\{"function":"github.com/satisfactorymodding/smr-api/logging.PrintLoggingDummies.func1","file":".*?/logging/logging_dummies.go","line":20},"msg":"C: 3"}`).Match(lines[2]))
+	testza.AssertTrue(t, regexp.MustCompile(`\{"time":".+?","level":"INFO","source":\{"function":"github.com/satisfactorymodding/smr-api/logging.PrintLoggingDummies.func2","file":".*?/logging/logging_dummies.go","line":27},"msg":"D: 4"}`).Match(lines[3]))
 
 	if t.Failed() {
 		println(buffer.String())
