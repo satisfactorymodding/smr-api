21f830ecad201eb945349ffac9872265b9a22f64
Rob B <computerguy440+gh@gmail.com>
2026-01-11 04:24:36 -0500

    fix: mise needs windows rewrites of the file tasks we use for migrations https://github.com/jdx/mise/discussions/7507

--- .mise/tasks/migrate_diff.ps1
+++ .mise/tasks/migrate_diff.ps1
@@ -1,8 +1,9 @@
-#!/usr/bin/env bash
 #MISE description="Use Atlas to attempt to automatically create SQL migration files (see https://atlasgo.io/versioned/diff)"
 
-echo -n 'Migration Name: '
-
-read -r migration_name
-
+Write-Host -NoNewline 'Migration Name: '
+$migration_name = Read-Host
+Write-Host "Atlas is running..."
 atlas migrate diff "$migration_name" --dir "file://migrations/sql?format=golang-migrate" --to "ent://db/schema" --dev-url "docker://postgres/16/test?search_path=public"
+if (-not $?) {
+	Write-Error "If the above failed due to a checksum error, don't run the command it suggests, instead run ``mise run migrate_hash`` "
+}
