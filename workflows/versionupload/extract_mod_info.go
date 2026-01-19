536b614ca582cf4bf010745c047e14e48a37f321
rhit-zhangl8 <112708732+rhit-zhangl8@users.noreply.github.com>
2026-01-19 16:43:25 -0500

    added new field and corresponding check

--- workflows/versionupload/extract_mod_info.go
+++ workflows/versionupload/extract_mod_info.go
@@ -78,6 +78,10 @@ func (*A) ExtractModInfoActivity(ctx context.Context, args ExtractModInfoArgs) (
 		return nil, temporal.NewNonRetryableApplicationError("reusing the version name of a deleted mod version is not allowed", "fatal", nil)
 	}
 
+	if mod.AiUseDisclosure.Type == "" {
+		return nil, temporal.NewNonRetryableApplicationError("ai use disclosure is required", "fatal", nil)
+	}
+
 	// Allow only new 5 versions per 24h
 	versions, err := db.From(ctx).Version.Query().
 		Order(version2.ByCreatedAt(sql.OrderAsc())).
