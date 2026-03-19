f2af725d8e900844830e6d5d2007a7a0840f7b36
rhit-mooretj <112582846+rhit-mooretj@users.noreply.github.com>
2026-03-19 02:16:14 -0400

    user modpacks should work

--- migrations/sql/20250623121418_add_modpacks.down.sql
+++ migrations/sql/20250623121418_add_modpacks.down.sql
@@ -1,14 +1,20 @@
 -- reverse: create index "modpacktarget_modpack_id_target_name" to table: "modpack_targets"
-DROP INDEX "modpacktarget_modpack_id_target_name";
+DROP INDEX IF EXISTS "modpacktarget_modpack_id_target_name";
+
 -- reverse: create "modpack_targets" table
-DROP TABLE "modpack_targets";
+DROP TABLE IF EXISTS "modpack_targets";
+
 -- reverse: create "modpack_tags" table
-DROP TABLE "modpack_tags";
+DROP TABLE IF EXISTS "modpack_tags";
+
 -- reverse: create index "modpackrelease_modpack_id_version" to table: "modpack_releases"
-DROP INDEX "modpackrelease_modpack_id_version";
+DROP INDEX IF EXISTS "modpackrelease_modpack_id_version";
+
 -- reverse: create "modpack_releases" table
-DROP TABLE "modpack_releases";
+DROP TABLE IF EXISTS "modpack_releases";
+
 -- reverse: create "modpack_mods" table
-DROP TABLE "modpack_mods";
+DROP TABLE IF EXISTS "modpack_mods";
+
 -- reverse: create "modpacks" table
-DROP TABLE "modpacks";
+DROP TABLE IF EXISTS "modpacks" CASCADE;
