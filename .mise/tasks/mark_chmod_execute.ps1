f858c3eb70cca404ce791a85cfd0b1cd0a4bc433
Rob B <computerguy440+gh@gmail.com>
2026-03-31 02:53:34 -0400

    chore: chmod +x linux scripts

---
+++ .mise/tasks/mark_chmod_execute.ps1
@@ -0,0 +1,9 @@
+#MISE description="For developers on Windows, mark all .sh file-based Mise tasks as executable so Linux devs and the CI can use them"
+
+$shFiles = Get-ChildItem -Path "./.mise/tasks" -Filter "*.sh" -File
+
+foreach ($file in $shFiles) {
+    Write-Host "Setting Linux execute permission on $($file.Name)"
+    # https://git-scm.com/docs/git-update-index#Documentation/git-update-index.txt---chmod-x
+    git update-index --chmod=+x $file.FullName
+}
