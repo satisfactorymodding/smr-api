2e04f04c74048f68a3df5e73e61e7a89fcc9f3e4
rhit-mooretj <112582846+rhit-mooretj@users.noreply.github.com>
2026-04-08 18:02:46 -0400

    feat: Implemented required components to allow for modpack compatibility editing

--- gql/directive.go
+++ gql/directive.go
@@ -28,6 +28,7 @@ func MakeDirective() generated.DirectiveRoot {
 		CanEditModCompatibility:     canEditModCompatibility,
 		CanEditSatisfactoryVersions: canEditSatisfactoryVersions,
 		CanEditModpack:              canEditModpack,
+		CanEditModpackCompatibility: canEditModpackCompatibility,
 	}
 }
 
@@ -266,6 +267,27 @@ func canEditModpack(ctx context.Context, _ interface{}, next graphql.Resolver, f
 	}
 
 	if db.UserHas(ctx, auth.RoleEditAnyContent, user) {
+		return next(ctx)
+	}
+
+	return nil, errors.New("user not authorized to perform this action")
+}
+
+func canEditModpackCompatibility(ctx context.Context, _ interface{}, next graphql.Resolver, field *string) (interface{}, error) {
+	user, _, err := db.UserFromGQLContext(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	if db.UserHas(ctx, auth.RoleEditAnyModCompatibility, user) || db.UserHas(ctx, auth.RoleEditAnyContent, user) {
+		return next(ctx)
+	}
+
+	if field == nil {
+		return nil, errors.New("user not authorized to perform this action")
+	}
+
+	if db.UserCanEditModpack(ctx, user, getArgument(ctx, *field).(string)) {
 		return next(ctx)
 	}
 
