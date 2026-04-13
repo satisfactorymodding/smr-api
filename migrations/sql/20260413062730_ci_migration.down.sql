eafbee6c16156c62056fe0b0af92722dc202755f
rhit-mooretj <112582846+rhit-mooretj@users.noreply.github.com>
2026-04-13 01:29:33 -0500

    chore: running commands to fix .sum issues

---
+++ migrations/sql/20260413062730_ci_migration.down.sql
@@ -0,0 +1,2 @@
+-- reverse: modify "modpack_targets" table
+ALTER TABLE "modpack_targets" DROP CONSTRAINT "modpack_targets_modpack_releases_targets", ADD CONSTRAINT "modpack_targets_modpacks_targets" FOREIGN KEY ("version_id") REFERENCES "modpack_releases" ("id") ON UPDATE NO ACTION ON DELETE CASCADE;
