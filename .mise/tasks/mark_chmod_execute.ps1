9e06d344eedbf8ddc4779862520a7bb86a9d1a27
Rob B <computerguy440+gh@gmail.com>
2026-04-27 19:43:11 -0400

    fix: `mark_chmod_execute` task can now add files to index

--- .mise/tasks/mark_chmod_execute.ps1
+++ .mise/tasks/mark_chmod_execute.ps1
@@ -5,5 +5,5 @@ $shFiles = Get-ChildItem -Path "./.mise/tasks" -Filter "*.sh" -File
 foreach ($file in $shFiles) {
     Write-Host "Setting Linux execute permission on $($file.Name)"
     # https://git-scm.com/docs/git-update-index#Documentation/git-update-index.txt---chmod-x
-    git update-index --chmod=+x $file.FullName
+    git update-index --add --chmod=+x $file.FullName
 }
