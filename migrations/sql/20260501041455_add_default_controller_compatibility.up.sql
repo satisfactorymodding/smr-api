105b873945b544ccb88bf7a1669bb09754582b3c
Rob B <computerguy440+gh@gmail.com>
2026-05-24 22:20:15 -0400

    refactor: simplified migration
    tested on staging db

--- migrations/sql/20260501041455_add_default_controller_compatibility.up.sql
+++ migrations/sql/20260501041455_add_default_controller_compatibility.up.sql
@@ -1,7 +1,5 @@
 UPDATE "mods"
-  SET "compatibility" = mods.compatibility || '{"Controller":""}'
-  WHERE NOT mods.compatibility ? 'Controller' AND mods.compatibility ? 'EA';
-
-UPDATE "mods"
+  -- Assign Controller compatibility of Untested...
   SET "compatibility" = jsonb_set(mods.compatibility, '{Controller}', '{"Note":"","State":"Untested"}'::jsonb)
-  WHERE mods.compatibility @> '{"Controller":""}'
+  -- ... to mods with pre-controller Compatibility already set + no leftover existing controller compatibility info
+  WHERE compatibility ? 'EA' and (NOT compatibility ? 'Controller');
